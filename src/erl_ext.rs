6204151760209e9747184d0b81159c3f72dbc718
Сергей Прохоров <seriy.pr@gmail.com>
2014-07-08 05:43:02 +0400

    Mostly all types are implemented * Floats * Port * Pid * Large_tuple * String * List * Small Big int * Funs

--- src/erl_ext.rs
+++ src/erl_ext.rs
@@ -12,7 +12,7 @@ use std::string::String;
 use std::vec::Vec;
 use std::num::FromPrimitive;
 use std::io;
-use num::bigint::BigInt;
+use num::bigint;
 use collections::bitv::Bitv;
 
 
@@ -65,10 +65,10 @@ pub enum Eterm {
     Tuple(Tuple),               // small_tuple, large_tuple
     Map(EMap),                  // map
     Nil,                        // nil
-    String(String),             // string
+    String(Vec<u8>),             // string XXX: maybe eliminate this in favour of List?
     List(List),                 // list
     Binary(Vec<u8>),            // binary
-    BigNum(BigInt),             // small_big, large_big
+    BigNum(bigint::BigInt),     // small_big, large_big
     Fun {                       // fun
         pid: Pid,
         module: Atom,
@@ -77,11 +77,11 @@ pub enum Eterm {
         free_vars: Vec<Eterm>},
     NewFun {                    // new_fun
         arity: u8,
-        uniq: u16,
+        uniq: Vec<u8>, //[u8, ..16],
         index: u32,
         module: Atom,
         old_index: u32,
-        old_uniq: u16,
+        old_uniq: u32,
         pid: Pid,
         free_vars: Vec<Eterm>},
     Export {                    // export
@@ -100,7 +100,7 @@ pub type List = Vec<Eterm>;
 #[deriving(Show)]
 pub struct Pid {                // moved out from enum because it used in Eterm::{Fun,NewFun}
     node: Atom,
-    id: u8,
+    id: u32,
     serial: u32,
     creation: u8,
 }
@@ -140,6 +140,28 @@ impl<T: io::Reader> Builder<T> {
                 Ok(SmallInteger(try!(self.rdr.read_u8()))),
             Some(INTEGER_EXT) =>
                 Ok(Integer(try!(self.rdr.read_be_int()))),
+            Some(FLOAT_EXT) => {
+                // XXX: there should be more elegant way...
+                let float_vec = try!(self.rdr.read_exact(31));
+                let float_str: &str = match std::str::from_utf8(float_vec.as_slice()) {
+                    Some(s) => s,
+                    None =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Bad utf-8",
+                            detail: None
+                        })
+                };
+                match from_str::<f32>(float_str) {
+                    Some(num) => Ok(Float(num as f64)),
+                    _ =>
+                        Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Bad float",
+                            detail: None // format!("{}", data)
+                        })
+                }
+            },
             Some(ATOM_EXT) | Some(ATOM_UTF8_EXT) => {
                 let len = try!(self.rdr.read_be_u16());
                 let data = try!(self.rdr.read_exact(len as uint));
@@ -152,7 +174,7 @@ impl<T: io::Reader> Builder<T> {
                             kind: io::OtherIoError,
                             desc: "Bad utf-8",
                             detail: None // format!("{}", data)
-                            })
+                        })
                 }
             },
             Some(REFERENCE_EXT) => {
@@ -174,15 +196,64 @@ impl<T: io::Reader> Builder<T> {
                     creation: creation
                 })
             },
+            Some(PORT_EXT) => {
+                let node = match try!(self.build()) {
+                    Atom(a) =>
+                        a,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Atom expected",
+                            detail: None
+                        })
+                };
+                let id = try!(self.rdr.read_exact(4));
+                let creation = try!(self.rdr.read_u8());
+                Ok(Reference {
+                    node: node,
+                    id: id,
+                    creation: creation
+                })
+            },
+            Some(PID_EXT) => {
+                let node = match try!(self.build()) {
+                    Atom(a) =>
+                        a,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Atom expected",
+                            detail: None
+                        })
+                };
+                let id = try!(self.rdr.read_be_u32());
+                let serial = try!(self.rdr.read_be_u32()); // maybe [u8, ..4]?
+                let creation = try!(self.rdr.read_u8());
+                Ok(Pid(Pid {
+                    node: node,
+                    id: id,
+                    serial: serial,
+                    creation: creation
+                }))
+            },
             Some(SMALL_TUPLE_EXT) => {
-                let mut tuple: Tuple = Vec::new();
                 let arity = try!(self.rdr.read_u8());
+                let mut tuple: Tuple = Vec::with_capacity(arity as uint);
                 for _ in range(0, arity) {
                     let term = try!(self.build());
                     tuple.push(term)
                 }
                 Ok(Tuple(tuple))
             },
+            Some(LARGE_TUPLE_EXT) => {
+                let arity = try!(self.rdr.read_be_u32());
+                let mut tuple: Tuple = Vec::with_capacity(arity as uint);
+                for _ in range(0, arity) {
+                    let term = try!(self.build());
+                    tuple.push(term)
+                }
+                Ok(Tuple(tuple))
+            }
             Some(MAP_EXT) => {
                 let mut map: EMap = Vec::new();
                 let arity = try!(self.rdr.read_be_u32());
@@ -195,10 +266,59 @@ impl<T: io::Reader> Builder<T> {
             },
             Some(NIL_EXT) =>
                 Ok(Nil),
+            Some(STRING_EXT) => {
+                let len = try!(self.rdr.read_be_u16());
+                Ok(String(try!(self.rdr.read_exact(len as uint))))
+            },
+            Some(LIST_EXT) => {
+                let len = try!(self.rdr.read_be_u32()) + 1;
+                let mut list = Vec::with_capacity(len as uint);
+                for _ in range(0, len) {
+                    let term = try!(self.build());
+                    list.push(term)
+                }
+                Ok(List(list))
+            },
             Some(BINARY_EXT) => {
                 let len = try!(self.rdr.read_be_u32());
                 Ok(Binary(try!(self.rdr.read_exact(len as uint))))
             },
+            Some(SMALL_BIG_EXT) => {
+                let n = try!(self.rdr.read_u8());
+                let sign_int = try!(self.rdr.read_u8());
+                let sign = if sign_int == 0 {
+                    bigint::Plus
+                } else {
+                    bigint::Minus
+                };
+                // In erlang:
+                // B = 256 % base is 2^8
+                // (d0*B^0 + d1*B^1 + d2*B^2 + ... d(N-1)*B^(n-1))
+                // In rust:
+                // BigDigit::base is 2^32
+                // (a + b * BigDigit::base + c * BigDigit::base^2)
+                let mut numbers = Vec::<u32>::with_capacity((n / 4) as uint);
+                let mut cur_num: u32 = 0;
+                for i in range(0, n) {
+                    let byte = try!(self.rdr.read_u8()) as u32;
+                    cur_num = match i % 4 {
+                        0 => cur_num + byte,
+                        1 => cur_num + byte * 256,
+                        2 => cur_num + byte * 65536,
+                        _ => {
+                            numbers.push(cur_num + byte * 16777216);
+                            0
+                        }
+                    }
+                }
+                if cur_num != 0 { // if 'n' isn't multiple of 4
+                    numbers.push(cur_num)
+                }
+                Ok(BigNum(bigint::BigInt::new(sign, numbers)))
+            },
+            // Some(LARGE_BIG_EXT) => {
+            // TODO
+            // },
             Some(NEW_REFERENCE_EXT) => {
                 let len = try!(self.rdr.read_be_u16());
                 let node = match try!(self.build()) {
@@ -234,6 +354,163 @@ impl<T: io::Reader> Builder<T> {
                             })
                 }
             },
+            Some(FUN_EXT) => {
+                // TODO: cleanup error handling (generalize)
+                let num_free = try!(self.rdr.read_be_u32());
+                let pid = match try!(self.build()) {
+                    Pid(pid) => pid,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Pid expected",
+                            detail: None
+                        })
+                };
+                let module = match try!(self.build()) {
+                    Atom(atom) => atom,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Atom expected",
+                            detail: None
+                        })
+                };
+                let index = match try!(self.build()) {
+                    SmallInteger(idx) => idx as u32,
+                    Integer(idx) => idx as u32,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Integer expected",
+                            detail: None
+                        })
+                };
+                let uniq = match try!(self.build()) {
+                    SmallInteger(uq) => uq as u32,
+                    Integer(uq) => uq as u32,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Integer expected",
+                            detail: None
+                        })
+                };
+                let mut free_vars = Vec::<Eterm>::with_capacity(num_free as uint);
+                for _ in range(0, num_free) {
+                    free_vars.push(try!(self.build()));
+                }
+                Ok(Fun {
+                    pid: pid,
+                    module: module,
+                    index: index,
+                    uniq: uniq,
+                    free_vars: free_vars,
+                })
+            },
+            Some(NEW_FUN_EXT) => {
+                let _size = try!(self.rdr.read_be_u32());
+                let arity = try!(self.rdr.read_u8());
+                let uniq = try!(self.rdr.read_exact(16));
+                let index = try!(self.rdr.read_be_u32());
+                let num_free = try!(self.rdr.read_be_u32());
+                // XXX: following code is copy-pasted!
+                let module = match try!(self.build()) {
+                    Atom(atom) => atom,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Atom expected",
+                            detail: None
+                        })
+                };
+                let old_index = match try!(self.build()) {
+                    SmallInteger(idx) => idx as u32,
+                    Integer(idx) => idx as u32,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Integer expected",
+                            detail: None
+                        })
+                };
+                let old_uniq = match try!(self.build()) {
+                    SmallInteger(uq) => uq as u32,
+                    Integer(uq) => uq as u32,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Integer expected",
+                            detail: None
+                        })
+                };
+                let pid = match try!(self.build()) {
+                    Pid(pid) => pid,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Pid expected",
+                            detail: None
+                        })
+                };
+                let mut free_vars = Vec::<Eterm>::with_capacity(num_free as uint);
+                for _ in range(0, num_free) {
+                    free_vars.push(try!(self.build()));
+                }
+                // END copy-pasted
+                Ok(NewFun {
+                    arity: arity,
+                    uniq: uniq,
+                    index: index,
+                    module: module,
+                    old_index: old_index,
+                    old_uniq: old_uniq,
+                    pid: pid,
+                    free_vars: free_vars,
+                })
+            },
+            Some(EXPORT_EXT) => {
+                let module = match try!(self.build()) {
+                    Atom(atom) => atom,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Atom expected",
+                            detail: None
+                        })
+                };
+                let function = match try!(self.build()) {
+                    Atom(atom) => atom,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: Atom expected",
+                            detail: None
+                        })
+                };
+                let arity = match try!(self.build()) {
+                    SmallInteger(uq) => uq,
+                    _ =>
+                        return Err(io::IoError{
+                            kind: io::OtherIoError,
+                            desc: "Assertion failed: SmallInteger expected",
+                            detail: None
+                        })
+                };
+                Ok(Export {
+                    module: module,
+                    function: function,
+                    arity: arity, // arity > u8 possible in practice
+                })
+            },
+            // Some(BIT_BINARY_EXT) => {
+            //     TODO
+            //     let len = try!(self.rdr.read_be_u32()) as uint;
+            //     let bits = try!(self.rdr.read_u8()) as uint;
+            //     let mut bitv = Bitv::new(len * 4 + bits); // XXX: uint overflow possible!
+            //     // unfortunately, bitv isn't resizable, so we couldn't use 'from_bytes'
+            // },
+            Some(NEW_FLOAT_EXT) =>
+                Ok(Float(try!(self.rdr.read_be_f64()))),
             Some(t) =>
                 Err(io::IoError{
                     kind: io::OtherIoError,
@@ -262,7 +539,7 @@ fn main() {
 
     let term: Eterm = NewFun {
         arity: 3,
-        uniq: 1234,
+        uniq: vec!(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16),
         index: 10,
         module: "my_mod".to_string(),
         old_index: 1212,
@@ -279,13 +556,7 @@ fn main() {
                         Tuple(vec!(Atom("record".to_string()), Map(map))),
                         ),
     };
-    println!("{:?}", term);
-    println!("==============================");
-    match term {
-        NewFun {free_vars: vars, ..} =>
-            println!("{:?}", vars.as_slice()),
-        _ => ()
-    }
+    println!("{}", term);
     println!("==============================");
     let f = BufferedReader::new(File::open(&Path::new("test/test_terms.bin")));
     let mut builder = Builder::new(f);
